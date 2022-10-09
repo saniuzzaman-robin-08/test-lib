ae30cf5c562304e9b4ffd6a9e4de3053dea1cda8
saniuzzaman-robin <saniuzzaman.robin@selise.ch>
2022-10-09 22:20:50 +0600

    init library project

---
+++ projects/test-lib/src/lib/test-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-test-lib',
+  templateUrl: './test-lib.component.html',
+  styles: [
+  ]
+})
+export class TestLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
